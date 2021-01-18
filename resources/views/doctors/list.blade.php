99679b208f74fc837d34ce54af55d38a9240d737
Gregori Mikina <gregorimikina@gmail.com>
2021-01-18 09:54:14 +0100

    .

--- resources/views/doctors/list.blade.php
+++ resources/views/doctors/list.blade.php
@@ -1,32 +1,32 @@
 @extends('template')
 
 @section('content')
-        <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Imię i Nazwisko</th>
-                                <th scope="col">E-mail</th>
-                                <th scope="col">Telefon</th>
-                                <th scope="col">Adres</th>
-                                <th scope="col">Status</th>
-                            </tr>
-                        </thead>
+    
+
+    <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Imię i Nazwisko</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Telefon</th>
+                    <th scope="col">Adres</th>
+                    <th scope="col">Status</th>
+                </tr>
+            </thead>
                         
-                        <tbody>
-                            @foreach ($doctorsList as $doctor)
-                            <tr>
-                                <th scope="row">{{ $doctor['id'] }}</th>
-                                <td>{{ $doctor['firstname'] }}{{ $doctor['lastname'] }}</td>
-                                <td>{{ $doctor['email'] }}</td>
-                                <td>{{ $doctor['phone'] }}</td>
-                                <td>{{ $doctor['address'] }}</td>
-                                <td>{{ $doctor['status'] }}</td>
-                            </tr>
-                            @endforeach
-                            
-                        
-        </tbody>
+            <tbody>
+                @foreach ($doctorsList as $doctor)
+                    <tr>
+                        <th scope="row">{{ $doctor['id'] }}</th>
+                        <td>{{ $doctor['firstname'] }}{{ $doctor['lastname'] }}</td>
+                        <td>{{ $doctor['email'] }}</td>
+                        <td>{{ $doctor['phone'] }}</td>
+                        <td>{{ $doctor['address'] }}</td>
+                        <td>{{ $doctor['status'] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
     </table>
 @endsection
 
