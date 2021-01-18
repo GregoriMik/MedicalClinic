455465c1baaf406db3e6891c60a74b020965005c
Gregori Mikina <gregorimikina@gmail.com>
2021-01-18 10:21:36 +0100

    Create links for css, js files Check if they work.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{  URL::asset('css/style.css')}}">
     <title>System obsługi @yield('title')</title>
   </head>
   <body>
@@ -39,7 +39,7 @@
     </footer>
 
     <!-- Optional JavaScript; choose one of the two! -->
-
+<script src="{{ URL::asset('js/main.js ') }}"
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
